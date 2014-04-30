e1db479c87f19d05d9bc6b84001fbcfed832c27b
jthom143 <jthom143@jhu.edu>
2014-04-30 14:21:55 -0400

    Edited code. Could not run because website is down. Want to add attribute data.

--- piControl_download.m
+++ piControl_download.m
@@ -12,6 +12,8 @@
 pathname = 'http://strega.ldeo.columbia.edu:81/CMIP5/.byScenario/.piControl/.atmos/.mon/.ps/.ACCESS1-0/.r1i1p1/.ps/lat/%2890S%29%280N%29RANGEEDGES/dods';
 
 ps_access0 = ncread(pathname, 'ps');
+time_access0 = ncread(pathname, 'T');
+lat_access0 = ncread(pathname, 'lat');
 
 clear pathname
 
@@ -20,6 +22,8 @@ clear pathname
 pathname = 'http://strega.ldeo.columbia.edu:81/CMIP5/.byScenario/.piControl/.atmos/.mon/.ps/.ACCESS1-3/.r1i1p1/.ps/dods';
 
 ps_access3 = ncread(pathname, 'ps');
+time_access3 = ncread(pathname, 'T');
+lat_access3 = ncread(pathname, 'lat');
 
 clear pathname
 
@@ -28,5 +32,7 @@ clear pathname
 pathname = 'http://strega.ldeo.columbia.edu:81/CMIP5/.byScenario/.piControl/.atmos/.mon/.ps/.bcc-csm1-1-m/.r1i1p1/.ps/dods';
 
 ps_bcc = ncread(pathname, 'ps');
+time_bcc = ncread(pathname, 'T');
+lat_bcc = ncread(pathname, 'lat');
 
 clear pathname
