8cdc64d797e7fe026c40e3da774304f8e30a4033
jthom143 <jthom143@jhu.edu>
2014-05-03 08:16:09 -0400

    New function: CMIP5_surface trends: Imports the data
    SSH into blaustein for data

--- CMIP5_surfacetrends.m
+++ CMIP5_surfacetrends.m
@@ -1,18 +1,29 @@
-function [ SAM_unnormalized, jet_ann, lat_jet_ann, taux_f, taux_xi, loc_f, loc_xi, sam_f, sam_xi, mean_taux, std_taux, mean_loc, std_loc, mean_sam, std_sam ] = CMIP5_surfacetrends(pathname, trend_period)
+function [ SAM_unnormalized, jet_ann, lat_jet_ann, taux_f, taux_xi, loc_f, loc_xi, sam_f, sam_xi, mean_taux, std_taux, mean_loc, std_loc, mean_sam, std_sam ] = CMIP5_surfacetrends(pathname_ps,pathname_taux, trend_period)
 % CM2Mc_surfacetrends.m loads CCSM windstress and sea level pressure data
 %      and calculates the surface trends to be compared to other models
 
 %% Load in Data
 
-ps = ncread(pathname, 'ps');    %Pa
-time = ncread(pathname, 'T');
-lat = ncread(pathname, 'lat');
+%sshfrommatlab_publickey('jthom143','blaustein.eps.jhu.edu','~/.ssh/authorized_keys', 'Harry Potter is my favorite book!')
+
+%pathname_ps = 'jthom143@blaustein.eps.jhu.edu:/data1/fesd1/jthom143/piControlData/CMIP5/CMCC_CESM_ps.cdf';
+%pathname_taux = 'jthom143@blaustein.eps.jhu.edu:/data1/fesd1/jthom143/piControlData/CMIP5/CMCC_CESM_taux.cdf';
+
+pathname_ps = '/data1/fesd1/jthom143/piControlData/CMIP5/CMCC_CESM_ps.cdf';
+pathname_ps = '/data1/fesd1/jthom143/piControlData/CMIP5/CMCC_CESM_taux.cdf';
+
+
+ps = ncread(pathname_ps, 'ps');    %Pa
+taux_mon = ncread(pathname_taux, 'tauuo');
+time = ncread(pathname_ps, 'T');
+lat_atmos = ncread(pathname_ps, 'lat');
+lat_ocean = ncread(pathname_taux, 'lat');
 
 clear pathname
 
 %% Change Units on Surface Pressure
 
-ps_mon = ps./100;    %hPa
+slp_mon = ps./100;    %hPa
 
 clear ps
 
@@ -26,7 +37,7 @@ clear time
 
 %% Wind Stress Calculations 
 
-[~, ~, ~, jet_ann, lat_jet_ann] = taux_findmax(taux_mon, lat, time_month);
+[~, ~, ~, jet_ann, lat_jet_ann] = taux_findmax(taux_mon, lat_ocean, time_month);
 
 %% SAM Calculations
 
