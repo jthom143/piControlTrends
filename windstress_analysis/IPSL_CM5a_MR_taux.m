a3baa2ea89ab77dc1b376defae797403abb4cbd2
jthom143 <jthom143@jhu.edu>
2014-06-25 11:11:35 -0400

    June 25, 2014

--- windstress_analysis/IPSL_CM5a_MR_taux.m
+++ windstress_analysis/IPSL_CM5a_MR_taux.m
@@ -1,56 +1,76 @@
-function [ jet_strength_f, jet_strength_xi, mean_jet_strength, std_jet_strength, jet_location_f, jet_location_xi, mean_jet_location, std_jet_location, time_year, jet_strength, jet location ] = IPSL_CM5a_MR_taux
+function [ jet_f, jet_xi, mean_jet, std_jet, jet_trends_yrs, jet_loc_f, jet_loc_xi, mean_jet_loc, std_jet_loc, jet_loc_trends_yrs, time_year ] = IPSL_CM5a_MR_taux
 
 %CMCC_CESM_taux loads in the windstress data for the CMCC CESM model and calculates the westerly wind jet and location and then calcuates the trend pdfs for these two variables.
 
+% %% Import Data
+% current_path = pwd;
+% pathname_taux = fullfile(current_path, 'piControlData/IPSL_CM5a_MR_taux.cdf');     % CDF file from Lamont website
+% pathname_coord = fullfile(current_path, 'piControlData/IPSL-CM5A-MR_tauuo_Omon__piControl_r1i1p1_180001-209912.nc');                      % CDF file from Lamont website does not contain lat and lon variables. 'pathname_coord' file downloaded from ESGF website to obtain these variables.
+% 
+% 
+% taux = ncread(pathname_taux,'tauuo');
+% time = ncread(pathname_taux,'T');
+% lat = ncread(pathname_coord, 'lat');
+% lon = ncread(pathname_coord, 'lon');
+% 
+% 
+% clear pathname_ps pathname_coord
+% 
+% %% Change from single type to double type
+% lat = double(lat);
+% lon = double(lon);
+% taux = double(taux);
+% 
+% %% Change land values to be NaNs
+% taux(taux>1e15)=NaN;
+% 
+% 
+% %% Plot Raw Data for first time step
+% figure(1)
+% axesm miller
+% surfacem(lat, lon, squeeze(taux(:,:,1)))
+% title('IPSL CM5a MR Surface Windstress -  Original Grid', 'fontsize', 12)
+% colorbar
+% set(gca, 'fontsize', 12)
+% 
+% %% Regridding
+% for i = 1:length(time);
+% [lon_new, lat_new, taux_new(:,:,i)] = griddata(lon, lat, squeeze(taux(:,:,i)), [0:360], [-90:90]');
+% end
+
 %% Import Data
-current_path = pwd;
-pathname_taux = fullfile(current_path, 'piControlData/IPSL_CM5a_MR_taux.cdf');     % CDF file from Lamont website
-pathname_coord = fullfile(current_path, 'piControlData/IPSL-CM5A-MR_tauuo_Omon__piControl_r1i1p1_180001-209912.nc');                      % CDF file from Lamont website does not contain lat and lon variables. 'pathname_coord' file downloaded from ESGF website to obtain these variables.
+load piControlData/ipsl_cm5a_mr_taux.mat
 
-
-taux = ncread(pathname_taux,'tauuo');
-time = ncread(pathname_taux,'T');
-lat = ncread(pathname_coord, 'lat');
-lon = ncread(pathname_coord, 'lon');
-
-
-clear pathname_ps pathname_coord
-
-%% Change from single type to double type
-lat = double(lat);
-lon = double(lon);
-taux = double(taux);
-
-%% Change land values to be NaNs
-taux(taux>1e15)=NaN;
-
-
-%% Plot Raw Data for first time step
-figure(1)
-axesm miller
-surfacem(lat, lon, squeeze(taux(:,:,1)))
-
-%% Regridding
-for i = 1:length(time);
-[lon_new, lat_new, taux_new(:,:,i)] = griddata(lon, lat, squeeze(taux(:,:,i)), [0:360], [-90:90]');
-end
+%% Plot Interpolated Data for first time step
+% figure(1)
+% axesm miller
+% surfacem(lat, lon, squeeze(taux(:,:,1)))
+% title('IPSL CM5a MR Surface Windstress -  Regridded', 'fontsize', 12)
+% colorbar
+% set(gca, 'fontsize', 12)
                                                
-                                               
-%% Create Useful time vectors
-                                               
+%% Create Useful time vectors                                               
                                                
 time_year = 1:300;
-                                               
-clear time
-                                               
+
+%% Create lat and lon vectors 
+lat_matrix = lat;
+lon_matrix = lon;
+
+clear lat lon
+
+lat = lat_matrix(:,1);
+lon = lon_matrix(1,:);
+
 %% Jet Strength and Location Analysis
-                                               
-                                               
+
+[ ~, ~, ~, jet_ann, lat_jet_ann ] = taux_findmax( taux, lat, time );                                              
+
 %% Trend Analysis PDFs
-                                               
- trend_period = 30;
- trend_length = 30;
-                                               
-[ sam_f, sam_xi, mean_sam, std_sam] = SAM_Trends( time_year, SAM_unnormalized, trend_period, trend_length );
-                                               
+
+trend_period = 30;
+trend_length = 30;
+
+[jet_f, jet_xi, mean_jet, std_jet, jet_trends_yrs, jet_loc_f, jet_loc_xi, mean_jet_loc, std_jet_loc, jet_loc_trends_yrs] = Windstress_Trends( time_year, jet_ann, lat_jet_ann, trend_period, trend_length );
+                                                 
 end
