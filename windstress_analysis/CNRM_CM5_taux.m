c13d03c32d7a16980a3106ee4e745d5c2c212746
jthom143-Blaustein <jthom143@jhu.edu>
2014-06-25 16:37:15 -0400

    Began working on function, taux is not the correct size -- come back to it

--- windstress_analysis/CNRM_CM5_taux.m
+++ windstress_analysis/CNRM_CM5_taux.m
@@ -1,37 +1,65 @@
+ function [ jet_f, jet_xi, mean_jet, std_jet, jet_trends_yrs, jet_loc_f, jet_loc_xi, mean_jet_loc, std_jet_loc, jet_loc_trends_yrs, time_year ] = CNRM_CM5_taux
+
 %CNRM_CM5_taux loads in the windstress data for the CNRM CM5 model and calculates the westerly wind jet and location and then calcuates the trend pdfs for these two variables. 
 
 close all
 clear all
 
 %% Import Data
-current_path = pwd;
-pathname_taux1 = fullfile(current_path, 'piControlData/CNRM_CM5_windstress/data(1).cdf');     % CDF file from Lamont website
-pathname_taux2 = fullfile(current_path, 'piControlData/CNRM_CM5_windstress/data.cdf');     % CDF file from Lamont website
-pathname_taux3 = fullfile(current_path, 'piControlData/CNRM_CM5_windstress/data(2).cdf');     % CDF file from Lamont website
+% current_path = pwd;
+% pathname_taux1 = fullfile(current_path, 'piControlData/CNRM_CM5_windstress/data(1).cdf');     % CDF file from Lamont website
+% pathname_taux2 = fullfile(current_path, 'piControlData/CNRM_CM5_windstress/data.cdf');     % CDF file from Lamont website
+% pathname_taux3 = fullfile(current_path, 'piControlData/CNRM_CM5_windstress/data(2).cdf');     % CDF file from Lamont website
+% 
+% %pathname_coord = fullfile(current_path, 'piControlData/');                      % CDF file from Lamont website does not contain lat and lon variables. 'pathname_coord' file downloaded from ESGF website to obtain these variables.
+% 
+% 
+% taux1 = ncread(pathname_taux1,'tauuo'); %N/m^2
+% taux2 = ncread(pathname_taux2,'tauuo');
+% taux3 = ncread(pathname_taux3,'tauuo');
+% 
+% time1 = ncread(pathname_taux1,'T');
+% time2 = ncread(pathname_taux2,'T');
+% time3 = ncread(pathname_taux3,'T');
+% 
+% %lat = ncread(pathname_coord, 'lat');
+% %lon = ncread(pathname_coord, 'lon');
+% 
+% taux = cat(3, taux1, taux2, taux3);
+% time = cat(1, time1, time2, time3);
+% 
+% clear current_path pathname_taux1 pathname_taux2 pathname_taux3
+% clear taux1 taux2 taux3 time1 time2 time3
+% 
+% cd NewFiles
+% 
+% save('CNRM_CM5_windstress.mat', 'taux', 'time', '-v7.3');
+% cd /Users/jordanthomas/piControlTrends
 
-%pathname_coord = fullfile(current_path, 'piControlData/');                      % CDF file from Lamont website does not contain lat and lon variables. 'pathname_coord' file downloaded from ESGF website to obtain these variables.
+load piControlData/CNRM_CM5/CNRM_CM5_windstress.mat
+load lat_lon.mat
 
+%% Create Useful time vectors
+time_year = 8027;
 
-taux1 = ncread(pathname_taux1,'tauuo'); %N/m^2
-taux2 = ncread(pathname_taux2,'tauuo');
-taux3 = ncread(pathname_taux3,'tauuo');
+%% Create lat and lon vectors 
+lat_matrix = lat;
+lon_matrix = lon;
 
-time1 = ncread(pathname_taux1,'T');
-time2 = ncread(pathname_taux2,'T');
-time3 = ncread(pathname_taux3,'T');
+clear lat lon
 
-%lat = ncread(pathname_coord, 'lat');
-%lon = ncread(pathname_coord, 'lon');
+lat = lat_matrix(:,1);
+lon = lon_matrix(1,:);
 
-taux = cat(3, taux1, taux2, taux3);
-time = cat(1, time1, time2, time3);
+%% Jet Strength and Location Analysis
 
-clear current_path pathname_taux1 pathname_taux2 pathname_taux3
-clear taux1 taux2 taux3 time1 time2 time3
+[ ~, ~, ~, jet_ann, lat_jet_ann ] = taux_findmax( taux, lat, time );                                              
 
-cd NewFiles
+%% Trend Analysis PDFs
 
-save('CNRM_CM5_windstress.mat', 'taux', 'time', '-v7.3');
-cd /Users/jordanthomas/piControlTrends
+trend_period = 30;
+trend_length = 30;
 
+[jet_f, jet_xi, mean_jet, std_jet, jet_trends_yrs, jet_loc_f, jet_loc_xi, mean_jet_loc, std_jet_loc, jet_loc_trends_yrs] = Windstress_Trends( time_year, jet_ann, lat_jet_ann, trend_period, trend_length );
 
+end
