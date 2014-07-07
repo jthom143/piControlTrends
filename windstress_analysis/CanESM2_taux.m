2ec9aef44e7bf6e7801cfaa12737e05f55a52ccf
jthom143-Blaustein <jthom143@jhu.edu>
2014-07-07 16:25:01 -0400

    Completed functions with newly downloaded data

--- windstress_analysis/CanESM2_taux.m
+++ windstress_analysis/CanESM2_taux.m
@@ -1,48 +1,53 @@
+function [ jet_f, jet_xi, mean_jet, std_jet, jet_trends_yrs, jet_loc_f, jet_loc_xi, mean_jet_loc, std_jet_loc, jet_loc_trends_yrs, time_year, jet_ann, lat_jet_ann ] = CanESM2_taux
+
 %CanESM2_taux loads in the windstress data for the CMCC CESM model and calculates the westerly wind jet and location and then calcuates the trend pdfs for these two variables. 
 
 %% Import Data
-current_path = pwd;
-pathname_taux = fullfile(current_path, 'piControlData/CanESM2/CanESM2_taux.cdf');     % CDF file from Lamont website
+% current_path = pwd;
+% pathname_taux1 = fullfile(current_path, 'piControlData/CanESM2/data.cdf');     % CDF file from Lamont website
+% pathname_taux2 = fullfile(current_path, 'piControlData/CanESM2/data(1).cdf');     % CDF file from Lamont website
+% 
+% taux1 = ncread(pathname_taux1, 'tauuo');
+% taux2 = ncread(pathname_taux2, 'tauuo');
+% 
+% time1 = ncread(pathname_taux1, 'T');
+% time2 = ncread(pathname_taux2, 'T');
+% 
+% taux = cat(3, taux1, taux2);
+% time = cat(1, time1, time2);
+% 
+% lat = ncread('piControlData/CanESM2/data.cdf', 'lat');
+% lon = ncread('piControlData/CanESM2/data.cdf', 'lon');
+% 
+% 
+% 
+% clear current_path pathname_taux1 pathname_taux2 time1 time2 taux1 taux2
+% 
+% %% Change from single type to double type
+% lat = double(lat);
+% lon = double(lon);
+% taux = double(taux);
+% 
+% %% Change land values to be NaNs
+% taux(taux>1e15)=NaN;
+% 
+% save('CanESM2_windstress.mat', 'taux', 'time', 'lat', 'lon', '-v7.3');
 
-taux = ncread(pathname_taux, 'tauuo');
-time = ncread(pathname_taux, 'T');
+load piControlData/CanESM2/CanESM2_windstress.mat
 
-pathname_coord = fullfile(current_path, 'piControlData/CanESM2/tauuo_Omon_CCSM4_piControl_r1i1p1_025001-050012.nc' );                      % CDF file from Lamont website does not contain lat and lon variables. 'pathname_coord' file downloaded from ESGF website to obtain these variables.
+%% Create Useful time vectors
+time_year = 1:996;
 
-lat = ncread(pathname_coord, 'lat');
-lon = ncread(pathname_coord, 'lon');
+%% Jet Strength and Location Analysis
 
+[ ~, ~, ~, jet_ann, lat_jet_ann ] = taux_findmax( taux, lat, time );                                              
 
-clear current_path pathname_taux pathname_coord
+%% Trend Analysis PDFs
 
-%% Change from single type to double type
-lat = double(lat);
-lon = double(lon);
-taux = double(taux);
+trend_period = 30;
+trend_length = 30;
 
-%% Change land values to be NaNs
-taux(taux>1e15)=NaN;
+[jet_f, jet_xi, mean_jet, std_jet, jet_trends_yrs, jet_loc_f, jet_loc_xi, mean_jet_loc, std_jet_loc, jet_loc_trends_yrs] = Windstress_Trends( time_year, jet_ann, lat_jet_ann, trend_period, trend_length );
 
-%% Plot Raw Data for first time step
-figure(1)
-axesm miller
-surfacem(lat, lon, squeeze(taux(:,:,1)))
-title('CanESM2 Surface Windstress - Original Grid', 'fontsize', 12)
-colorbar
-set(gca, 'fontsize', 12)
-set(gcf, 'position', [100, 100, 1049, 895])
-
-%% Regridding
-for i = 1:length(time);
-[lon_new, lat_new, taux_new(:,:,i)] = griddata(lon, lat, squeeze(taux(:,:,i)), [0:360], [-90:90]');
 end
-
-clear lat lon taux
-
-lat = lat_new;
-lon = lon_new;
-taux = taux_new;
-
-save('CanESM2_windstress.mat', 'taux', 'time', '-v7.3');
-
 
